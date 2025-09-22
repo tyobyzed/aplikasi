2dde2bc548da4d35fdf48d72f34413617769df96
tyobyzed <bkabdulmalik20@gmail.com>
2025-09-22 18:27:29 +0700

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,9 @@
+// supabase-config.js
+const SUPABASE_URL = 'https://tahzvvghvnhimjrwlazd.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhaHp2dmdodm5oaW1qcndsYXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1MzkzNDUsImV4cCI6MjA3NDExNTM0NX0.UnXdclChG7BI8sX5dGpRdkyCnq37vCbYiVgqzXThla4';
+
+// Ganti dengan kredensial dari Supabase Anda
+// Cara mendapatkan:
+// 1. Buka project Supabase
+// 2. Klik Settings → API
+// 3. Copy Project URL dan anon public key
